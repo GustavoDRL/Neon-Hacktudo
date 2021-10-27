0612a6240a40a6bed877c411ad2090d169562066
GustavoDRL <gustavo.rio@aluno.ufabc.edu.br>
2021-10-27 06:08:03 -0300

    Codigo doente e comentarios fairy

--- Codigos/doente/doente.ino
+++ Codigos/doente/doente.ino
@@ -1,31 +1,31 @@
+  //Biblioteca do controle de PS4
   #include <PS4Controller.h>
-  
+ 
+  //Locomocão 
   #define PWMA 27
   #define PWMB 26
   #define A1  13
   #define A2  12
   #define B1  25
   #define B2  33
+
   
+  int inv = 1; //Permite inverter a pilotagem conforme o lado do robo que esta para cima
   
-  #include <Servo_ESP32.h>
-  static const int brushPin = 14; 
-  Servo_ESP32 servo;
-  int angle;
-  int inv = 1; //permite inverter a pilotagem conforme o lado do robo que esta para cima
   void motors_control(int linear, int angular) {
     int result_R = linear - angular; //ao somar o angular com linear em cada motor conseguimos a ideia de direcao do robo
     int result_L = linear + angular;
-    
-    if(result_R<15 && result_R >-15) result_R=0; //não envia valores de potencia abaixo de 15, que não são fortes o suficiente para mover o robo
+    //não envia valores de potencia abaixo de 15, que não são fortes o suficiente para mover o robo
+    if(result_R<15 && result_R >-15) result_R=0; 
     if(result_L<15 && result_L >-15 ) result_L=0;
-    if(result_R >255 ) result_R= 254; //nao permite valores superiores a 255 ou inferiores a -255
+    //Não permite valores superiores a 255 ou inferiores a -255
+    if(result_R >255 ) result_R= 254; 
     if(result_R<-255 ) result_R=-254;
     if(result_L >255 ) result_L= 254;
     if(result_L<-255 ) result_L=-254;  
     
-      
-    motor_A(result_R); //manda para a funcao motor um valor de -255 a 255, o sinal signifca a direcao
+    //manda para a funcao motor um valor de -255 a 255, o sinal signifca a direcao  
+    motor_A(result_R); 
     motor_B(result_L);
   }
   
@@ -81,9 +81,6 @@
     digitalWrite(B1, 0);
     digitalWrite(B2, 0);
   
-   servo.attach(brushPin);
-   int angle = 0;
-   servo.write(angle);
    while(PS4.isConnected()!= true){
     delay(20);}
   }
@@ -91,58 +88,33 @@
   void loop() {
   
     while(PS4.isConnected()) {
-    // estou multiplicando por 1.8 para aumentar a velocidade linear, o quao rapido o robo vai ser
-    // estou dividindo por 2 o angular, para o robô fazer menos curva e ser mais facil de controlar
-    
-    //motors_control(linear_speed*multiplicador, angular_speed* multiplicador2); 
+    //motors_control(linear_speed*multiplicador, angular_speed* multiplicador2);
+    // Multiplicadcor = 1.8 para aumentar a velocidade linear, o quao rapido o robo vai ser
+    // Multiplicadcor2 = multiplic_curva, parametro que varia de 1 ate a 2.3 para suavisar as curvas em alta velocidade
       if(PS4.LStickY()<-15 || PS4.LStickY()>15){
         float multiplic_curva = map(PS4.LStickY(), -124, 125, 1, 2.3);
         motors_control(1.8*inv*PS4.LStickY(), (-1)*PS4.RStickX()/multiplic_curva);
-      }else { // Controle sobre valores pequenos que bugam a funcao map
+      }else { // Controle sobre valores pequenos devido a problemas na funcao map
         motors_control(1.8*inv*PS4.LStickY(), (-1)*PS4.RStickX());
       }
       
-        
-      if (PS4.Cross()) { 
-          angle=40;
-          servo.write(angle);
-          Serial.println(angle);
-          delay(20);
-      }
-  
+        //Sentido de locomocao invertido
        if(PS4.R3()){
           inv = -1;
           delay(100);
        }
+       //Sentido de locomocao principal
         if(PS4.L3()){
           inv = 1;
           delay(100);
        }
-       if(PS4.R1()){
-          servo.detach();
-          delay(100);
-          servo.attach(brushPin);
-          angle = 40;
-          servo.write(angle);
-          Serial.println(angle);
-  
-       }else if(PS4.R2()){
-          if(PS4.R2Value()>15){
-            angle = map(PS4.R2Value(), 0, 255, 20, 140);
-          }else{
-            angle = 40;
-          }
-                  
-          servo.write(angle);
-          Serial.println(angle);
-       }   
-    } 
-    if(PS4.isConnected()!= true){
-    angle = 40;
-    servo.write(angle);
+       
+    }
+     
+    //Failsafe
+    if(PS4.isConnected()!= true){;
     motors_control(0,0);
     Serial.println("Restart");
-    setup();
     delay(20);
     }
     
